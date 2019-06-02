aae3d67f9f45630e7c7102592c56976cc84f9b3e
Kawano <kawano.misato@gmail.com>
2019-06-02 21:55:22 +0900

    Hello React+Firebase! Let's enjoy

--- src/Home.js
+++ src/Home.js
@@ -102,7 +102,7 @@ function ComplexGrid(props) {
      <Grid container justify="center" alignItems="center" direction="row" className={classes.root}>
      <Grid className={classes.caption}>
           <Typography component="h2" variant="h5" gutterBottom>
-          Hello React+Firebase! 
+          Hello React+Firebase! Let's enjoy
           </Typography>
           </Grid>
    <div className={classes2.root}>
